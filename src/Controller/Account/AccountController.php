7633682d486f44ecdb5d687c792727e434115821
0xArthem <82900721+0xArthem@users.noreply.github.com>
2023-04-24 21:42:19 +0200

    5 - Design du formulaire EditProfil, ajout d'une fenêtre modale

--- src/Controller/Account/AccountController.php
+++ src/Controller/Account/AccountController.php
@@ -19,51 +19,42 @@ class AccountController extends AbstractController
     /**
      * @Route("/", name="app_account")
      */
-    public function index(OrderRepository $repoOrder, OrderDetailsRepository $repoOrderDetails): Response
+    public function index(OrderRepository $repoOrder, OrderDetailsRepository $repoOrderDetails, Request $request): Response
     {
         $orders = $repoOrder->findBy(['isPaid' => true, 'user' => $this->getUser()], ['id' => 'DESC'], null, null, ['orderDetails']);
 
+         // récupère l'utilisateur connecté
+         $user = $this->getUser();
+         // crée le formulaire et le lie à l'utilisateur
+         $form = $this->createForm(EditProfilType::class, $user);
+         $form->handleRequest($request);
+ 
+         if ($form->isSubmitted() && $form->isValid()) {
+ 
+             // gérer l'upload de l'image de profil
+             $imageFile = $form->get('image')->getData();
+             if ($imageFile) {
+                 $newFilename = uniqid().'.'.$imageFile->guessExtension();
+ 
+                 try {
+                     $imageFile->move(
+                         $this->getParameter('profile_images_directory'),
+                         $newFilename
+                     );
+                 } catch (FileException $e) {
+                     // gérer les erreurs d'upload ici
+                 }
+ 
+                 $user->setImage($newFilename);
+             }
+ 
+             $this->getDoctrine()->getManager()->flush();
+ 
+             return $this->redirectToRoute('app_account');
+         }
+
         return $this->render('account/index.html.twig', [
-            'orders' => $orders
-        ]);
-    }
-
-    /**
-     * @Route("/modifier-profil", name="app_account_edit_profil")
-     */
-    public function editProfile(Request $request): Response
-    {
-        // récupère l'utilisateur connecté
-        $user = $this->getUser();
-        // crée le formulaire et le lie à l'utilisateur
-        $form = $this->createForm(EditProfilType::class, $user);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-
-            // gérer l'upload de l'image de profil
-            $imageFile = $form->get('image')->getData();
-            if ($imageFile) {
-                $newFilename = uniqid().'.'.$imageFile->guessExtension();
-
-                try {
-                    $imageFile->move(
-                        $this->getParameter('profile_images_directory'),
-                        $newFilename
-                    );
-                } catch (FileException $e) {
-                    // gérer les erreurs d'upload ici
-                }
-
-                $user->setImage($newFilename);
-            }
-
-            $this->getDoctrine()->getManager()->flush();
-
-            return $this->redirectToRoute('app_account');
-        }
-
-        return $this->render('account/edit_profile.html.twig', [
+            'orders' => $orders,
             'form' => $form->createView(),
         ]);
     }
--- src/Form/EditProfilType.php
+++ src/Form/EditProfilType.php
@@ -7,20 +7,54 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class EditProfilType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('username')
-            ->add('bio')
-            ->add('localisation')
-            ->add('isPublic')
+            ->add('username', TextType::class, [
+                'label' => false,
+                'attr' => [
+                    'class' => 'form-control',
+                ],
+            ])
+            ->add('bio', TextareaType::class, [
+                'label' => false,
+                'attr' => [
+                    'class' => 'form-control textarea-edit-profil',
+                ],
+            ])
+            ->add('localisation', TextType::class, [
+                'label' => false,
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Localisation',
+                ],
+            ])
+            ->add('isPublic', null, [
+                'label' => 'Profil public',
+                'label_attr' => [
+                    'class' => 'me-2'
+                ]
+            ])
             ->add('image', FileType::class, [
-                'label' => 'Image de profil (JPEG, PNG, GIF)',
+                'label' => false,
                 'mapped' => false,
                 'required' => false,
+                'attr' => [
+                    'class' => 'form-control',
+                ],
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Enregistrer',
+                'attr' => [
+                    'class' => 'btn btn-dark',
+                ],
             ])
         ;
     }
