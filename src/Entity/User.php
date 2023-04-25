b2de8a5df86841218bbadfd7062f768039425f95
0xArthem <82900721+0xArthem@users.noreply.github.com>
2023-04-25 11:58:53 +0200

    7 - Ajout de la contrainte UniqueEntity sur username et email

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -12,7 +12,8 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
- * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
+ * @UniqueEntity(fields={"email"}, message="Un compte avec cette adresse électronique existe déjà.")
+ * @UniqueEntity(fields={"username"}, message="Ce pseudo est déjà utilisé.")
  */
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
@@ -40,7 +41,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private $password;
 
     /**
-     * @ORM\Column(type="string", length=20)
+     * @ORM\Column(type="string", length=20, unique=true)
      */
     private $username;
 
