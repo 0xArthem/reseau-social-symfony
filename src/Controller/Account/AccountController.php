e51d00d7f6f5e0278a078db3a7bb1c492f846763
0xArthem <82900721+0xArthem@users.noreply.github.com>
2023-04-25 12:53:00 +0200

    9 - Affichage des posts dans /account par ordre decroissant et design

--- src/Controller/Account/AccountController.php
+++ src/Controller/Account/AccountController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Account;
 use App\Form\EditProfilType;
 use App\Repository\OrderRepository;
 use App\Repository\OrderDetailsRepository;
+use App\Repository\PostRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -19,7 +20,7 @@ class AccountController extends AbstractController
     /**
      * @Route("/", name="app_account")
      */
-    public function index(OrderRepository $repoOrder, OrderDetailsRepository $repoOrderDetails, Request $request): Response
+    public function index(PostRepository $postRepository, OrderRepository $repoOrder, OrderDetailsRepository $repoOrderDetails, Request $request): Response
     {
         $orders = $repoOrder->findBy(['isPaid' => true, 'user' => $this->getUser()], ['id' => 'DESC'], null, null, ['orderDetails']);
 
@@ -59,10 +60,12 @@ class AccountController extends AbstractController
             $this->addFlash('error', 'Votre profil n\'a pas pu être correctement mis à jour.');
             // return $this->redirectToRoute('app_account');
         }
+        $posts = $postRepository->findBy(array(), array('id' => 'DESC'));
 
         return $this->render('account/index.html.twig', [
             'orders' => $orders,
             'form' => $form->createView(),
+            'posts' => $posts
         ]);
     }
 }
