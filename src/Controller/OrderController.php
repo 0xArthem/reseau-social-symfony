ca4a56dc14cb89e45f5cacd6d80ea5d9600ec1ce
0xArthem <82900721+0xArthem@users.noreply.github.com>
2023-06-01 14:58:01 +0200

    86 - E-commerce : Création de l'Entity Order, mise en place de prepareOrder()

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Form\OrderType;
 use App\Services\CartServices;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -26,6 +27,27 @@ class OrderController extends AbstractController
         return $this->render('order/index.html.twig', [
             'form' => $form->createView(),
             'cart' => $cartServices->getTotal()
+        ]);
+    }
+
+    /**
+     * @Route("/order/prepare", name="order_prepare", methods={"POST"})
+     */
+    public function prepareOrder(Request $request): Response {
+        if (!$this->getUser()) {
+            return $this->redirectToRoute('app_login');
+        }
+
+        $form = $this->createForm(OrderType::class, null, [
+            'user' => $this->getUser()
+        ]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $datetime = new \DateTime('now');
+        }
+
+        return $this->render('order/recap.html.twig', [
         ]);
     }
 }
---
+++ src/Entity/Order.php
@@ -0,0 +1,201 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\OrderRepository;
+use DateTime;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=OrderRepository::class)
+ * @ORM\Table(name="`order`")
+ */
+class Order
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="orders")
+     */
+    private $user;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $transporterName;
+
+    /**
+     * @ORM\Column(type="float", nullable=true)
+     */
+    private $transporterPrice;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $delivery;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isPaid;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $method;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $reference;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $stripeSessionId;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $paypalOrderId;
+
+    // public function __construct()
+    // {
+    //     $this->createdAt = new DateTime();
+    // }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getTransporterName(): ?string
+    {
+        return $this->transporterName;
+    }
+
+    public function setTransporterName(?string $transporterName): self
+    {
+        $this->transporterName = $transporterName;
+
+        return $this;
+    }
+
+    public function getTransporterPrice(): ?float
+    {
+        return $this->transporterPrice;
+    }
+
+    public function setTransporterPrice(?float $transporterPrice): self
+    {
+        $this->transporterPrice = $transporterPrice;
+
+        return $this;
+    }
+
+    public function getDelivery(): ?string
+    {
+        return $this->delivery;
+    }
+
+    public function setDelivery(?string $delivery): self
+    {
+        $this->delivery = $delivery;
+
+        return $this;
+    }
+
+    public function isIsPaid(): ?bool
+    {
+        return $this->isPaid;
+    }
+
+    public function setIsPaid(bool $isPaid): self
+    {
+        $this->isPaid = $isPaid;
+
+        return $this;
+    }
+
+    public function getMethod(): ?string
+    {
+        return $this->method;
+    }
+
+    public function setMethod(?string $method): self
+    {
+        $this->method = $method;
+
+        return $this;
+    }
+
+    public function getReference(): ?string
+    {
+        return $this->reference;
+    }
+
+    public function setReference(string $reference): self
+    {
+        $this->reference = $reference;
+
+        return $this;
+    }
+
+    public function getStripeSessionId(): ?string
+    {
+        return $this->stripeSessionId;
+    }
+
+    public function setStripeSessionId(?string $stripeSessionId): self
+    {
+        $this->stripeSessionId = $stripeSessionId;
+
+        return $this;
+    }
+
+    public function getPaypalOrderId(): ?string
+    {
+        return $this->paypalOrderId;
+    }
+
+    public function setPaypalOrderId(?string $paypalOrderId): self
+    {
+        $this->paypalOrderId = $paypalOrderId;
+
+        return $this;
+    }
+}
