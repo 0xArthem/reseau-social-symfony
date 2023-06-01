d0d35317d680f8727963448c18012c4622c186ae
0xArthem <82900721+0xArthem@users.noreply.github.com>
2023-06-01 15:16:06 +0200

    87 - E-commerce : Méthode prepareOrder()

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -2,15 +2,24 @@
 
 namespace App\Controller;
 
+use App\Entity\Order;
 use App\Form\OrderType;
 use App\Services\CartServices;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class OrderController extends AbstractController
 {
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
     /**
      * @Route("/order/create", name="order_index")
      */
@@ -45,6 +54,33 @@ class OrderController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             $datetime = new \DateTime('now');
+            $transporter = $form->get('transporter')->getData();
+
+            $delivery = $form->get('addresses')->getData();
+            $deliveryForOrder = $delivery->getFirstName().' '.$delivery->getLastName();
+            $deliveryForOrder .= '</br>' . $delivery->getPhone();
+            if ($delivery->getCompany()) {
+                $deliveryForOrder .= ' - ' . $delivery->getCompany();
+            }
+            $deliveryForOrder .= '</br>' . $delivery->getAddress();
+            $deliveryForOrder .= '</br>' . $delivery->getPostalCode() . ' ' . $delivery->getCity();
+            $deliveryForOrder .= '</br>' . $delivery->getCountry();
+            // dd($deliveryForOrder);
+            
+            $order = new Order();
+            $reference = $datetime->format('dmY').'-'.uniqid();
+            $order->setReference($reference);
+            $order->setUser($this->getUser());
+            $order->setCreatedAt($datetime);
+            $order->setDelivery($deliveryForOrder);
+            $order->setTransporterName($transporter->getTitle());
+            $order->setTransporterPrice($transporter->getPrice());
+            $order->setIsPaid(0);
+
+            $order->setMethod('stripe');
+
+            $this->em->persist($order);
+            $this->em->flush();
         }
 
         return $this->render('order/recap.html.twig', [
