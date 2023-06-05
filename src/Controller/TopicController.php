d71b1ed9743955df175c1eecd86665638abb4a2d
0xArthem <82900721+0xArthem@users.noreply.github.com>
2023-06-05 22:10:29 +0200

    110 - Ajout de la panigation avec KnpPaginatorBundle sur les commentaires

--- src/Controller/TopicController.php
+++ src/Controller/TopicController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use App\Entity\Post;
 use App\Entity\Topic;
 use App\Repository\TopicRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -14,7 +16,7 @@ class TopicController extends AbstractController
     /**
      * @Route("/commentaires/{id}", name="app_topic_show")
      */
-    public function show($id, TopicRepository $topicRepository): Response
+    public function show($id, TopicRepository $topicRepository, Request $request, PaginatorInterface $paginator): Response
     {
         $topic = $topicRepository->find($id);
         if (!$topic) {
@@ -23,7 +25,15 @@ class TopicController extends AbstractController
 
         $post = $topic->getPost();
 
-        $commentaires = $topic->getCommentaires();
+        $stockCommentaires = $topic->getCommentaires();
+
+        $query = $stockCommentaires;
+        $page = $request->query->get('page', 1);
+        $commentaires = $paginator->paginate(
+            $query,
+            $page,
+            15
+        );
 
         return $this->render('topic/index.html.twig', [
             'topic' => $topic,
