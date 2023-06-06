f2b90c64219c0f5e4593650b44c057647ed761d6
0xArthem <82900721+0xArthem@users.noreply.github.com>
2023-06-06 11:30:43 +0200

    114 - Récupération des commentaires par le slug du post

--- src/Controller/TopicController.php
+++ src/Controller/TopicController.php
@@ -6,6 +6,7 @@ use App\Entity\Post;
 use App\Entity\Topic;
 use App\Entity\Commentaire;
 use App\Form\CommentaireType;
+use App\Repository\PostRepository;
 use App\Repository\TopicRepository;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -16,19 +17,21 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class TopicController extends AbstractController
 {
     /**
-     * @Route("/{id}/commentaires", name="app_topic_show")
+     * @Route("/commentaires/{slug}", name="app_topic_show")
      */
-    public function show($id, TopicRepository $topicRepository, Request $request, PaginatorInterface $paginator): Response
+    public function show($slug, PostRepository $postRepository, Request $request, PaginatorInterface $paginator): Response
     {
-        $topic = $topicRepository->find($id);
+        $post = $postRepository->findOneBySlug($slug);
+        if (!$post) {
+            return $this->redirectToRoute('app_home');
+        }
+        $topic = $post->getTopic();
         if (!$topic) {
             return $this->redirectToRoute('app_home');
         }
-
         $post = $topic->getPost();
 
         $stockCommentaires = $topic->getCommentaires();
-
         $query = $stockCommentaires;
         $page = $request->query->get('page', 1);
         $commentaires = $paginator->paginate(
@@ -40,16 +43,14 @@ class TopicController extends AbstractController
         $commentaire = new Commentaire();
         $commentaire->setTopic($topic);
         $commentaire->setUser($this->getUser());
-
         $commentaireForm = $this->createForm(CommentaireType::class, $commentaire);
-
         $commentaireForm->handleRequest($request);
         if ($commentaireForm->isSubmitted() && $commentaireForm->isValid()) {
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($commentaire);
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_topic_show', ['id' => $id]);
+            return $this->redirectToRoute('app_topic_show', ['slug' => $slug]);
         }
         if ($commentaireForm->isSubmitted() && !$commentaireForm->isValid()) {
             $this->addFlash('error', 'Oups, votre commentaire n\'a pas pu être envoyé. Veuillez réessayer.');
