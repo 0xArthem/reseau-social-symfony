562fda7c60eb72068028a733248f8bb639bc0115
0xArthem <82900721+0xArthem@users.noreply.github.com>
2023-06-08 15:10:26 +0200

    122 - E-commerce : Implémentation de Stripe Part.3

--- src/Controller/PaymentController.php
+++ src/Controller/PaymentController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Order;
 use App\Entity\Product;
+use App\Services\CartServices;
 use Doctrine\ORM\EntityManagerInterface;
 use Stripe\Stripe;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,15 +12,18 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class PaymentController extends AbstractController
 {
 
     private $em;
+    private $generator;
 
-    public function __construct(EntityManagerInterface $em)
+    public function __construct(EntityManagerInterface $em, UrlGeneratorInterface $url)
     {
         $this->em = $em;
+        $this->generator = $url;
     }
 
     /**
@@ -45,37 +49,70 @@ class PaymentController extends AbstractController
                     'product_data' => [
                         'name' => $product->getProduct()
                     ],
-                    'quantity' => $product->getQuantity(),
-                ]
+                ],
+                'quantity' => $product->getQuantity(),
             ];
         }
 
         $productStripe[] = [
             'price_data' => [
                 'currency' => 'eur',
-                'unit_amount' => $order->getTransporterPrice(),
+                'unit_amount' => $order->getTransporterPrice()*100,
                 'product_data' => [
-                    'name' => $product->getProduct()
+                    'name' => $order->getTransporterName(),
                 ],
-                'quantity' => $order->getTransporterName(),
-            ]
+            ],
+            'quantity' => 1,
         ];
 
-        dd($productStripe);
+        // dd($productStripe);
 
         Stripe::setApiKey('sk_test_51M3KrLJysBhvIGnlEYRSx3rY9tXPoyJy2DZ8meJWaByaMO2A13ne45OjjDNmjfB1j8YDgAWCUe5mUzKAfxkLg5Jc001tEfq9X8');
-
+        // dd($checkout_session);
         $checkout_session = \Stripe\Checkout\Session::create([
             // 'customer_email' => $this->getUser()->getEmail(),
             'payment_method_types' => ['card'],
-            'line_items' => [[
-              $productStripe
-            ]],
+            'line_items' => $productStripe,
             'mode' => 'payment',
-            // 'success_url' => $YOUR_DOMAIN . '/success.html',
-            // 'cancel_url' => $YOUR_DOMAIN . '/cancel.html',
+            'success_url' => $this->generator->generate('payment_success', [
+                'reference' => $order->getReference()
+            ], UrlGeneratorInterface::ABSOLUTE_URL),
+            'cancel_url' => $this->generator->generate('payment_error', [
+                'reference' => $order->getReference()
+            ], UrlGeneratorInterface::ABSOLUTE_URL),
         ]);
 
+        $order->setStripeSessionId($checkout_session->id);
+        $this->em->flush();
+
         return new RedirectResponse($checkout_session->url);
+    }
+
+    /**
+     * @Route("/panier/success/{reference}", name="payment_success")
+     */
+    public function StripeSuccess($reference, CartServices $cartServices): Response {
+        $order = $this->em->getRepository(Order::class)->findOneBy(['reference' => $reference]);
+        if (!$order || $order->getUser() !== $this->getUser()) {
+            return $this->redirectToRoute('cart_index');
+        }
+        if (!$order->isIsPaid()) {
+            $cartServices->removeCartAll();
+            $order->setIsPaid(1);
+            $this->em->flush();
+        }
+        return $this->render('order/success.html.twig', [
+            'order' => $order
+        ]);
+    }
+
+    /**
+     * @Route("/panier/error/{reference}", name="payment_error")
+     */
+    public function StripeError($reference): Response {
+        $order = $this->em->getRepository(Order::class)->findOneBy(['reference' => $reference]);
+        return $this->render('order/error.html.twig', [
+            'order' => $order
+        ]);
     }
 }
