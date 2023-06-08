d6ab484ea0116530ce86d2790a3161b78effb43c
0xArthem <82900721+0xArthem@users.noreply.github.com>
2023-06-08 11:08:19 +0200

    121 - E-commerce : Implémentation de Stripe Part.2

--- src/Controller/PaymentController.php
+++ src/Controller/PaymentController.php
@@ -3,12 +3,14 @@
 namespace App\Controller;
 
 use App\Entity\Order;
+use App\Entity\Product;
 use Doctrine\ORM\EntityManagerInterface;
 use Stripe\Stripe;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class PaymentController extends AbstractController
 {
@@ -26,24 +28,54 @@ class PaymentController extends AbstractController
     public function stripeCheckout($reference): Response
     {
 
+        $productStripe = [];
+
         $order = $this->em->getRepository(Order::class)->findOneBy(['reference' => $reference]);
-        dd($order);
+        if (!$order) {
+            return $this->redirectToRoute('cart_index');
+        }
+
+        foreach ($order->getRecapDetails()->getValues() as $product) {
+            $productData = $this->em->getRepository(Product::class)->findOneBy(['title' => $product->getProduct()]);
+            // dd($productData);
+            $productStripe[] = [
+                'price_data' => [
+                    'currency' => 'eur',
+                    'unit_amount' => $productData->getPrice(),
+                    'product_data' => [
+                        'name' => $product->getProduct()
+                    ],
+                    'quantity' => $product->getQuantity(),
+                ]
+            ];
+        }
+
+        $productStripe[] = [
+            'price_data' => [
+                'currency' => 'eur',
+                'unit_amount' => $order->getTransporterPrice(),
+                'product_data' => [
+                    'name' => $product->getProduct()
+                ],
+                'quantity' => $order->getTransporterName(),
+            ]
+        ];
+
+        dd($productStripe);
 
         Stripe::setApiKey('sk_test_51M3KrLJysBhvIGnlEYRSx3rY9tXPoyJy2DZ8meJWaByaMO2A13ne45OjjDNmjfB1j8YDgAWCUe5mUzKAfxkLg5Jc001tEfq9X8');
 
         $checkout_session = \Stripe\Checkout\Session::create([
+            // 'customer_email' => $this->getUser()->getEmail(),
+            'payment_method_types' => ['card'],
             'line_items' => [[
-              # Provide the exact Price ID (e.g. pr_1234) of the product you want to sell
-              'price' => '{{PRICE_ID}}',
-              'quantity' => 1,
+              $productStripe
             ]],
             'mode' => 'payment',
-            'success_url' => $YOUR_DOMAIN . '/success.html',
-            'cancel_url' => $YOUR_DOMAIN . '/cancel.html',
+            // 'success_url' => $YOUR_DOMAIN . '/success.html',
+            // 'cancel_url' => $YOUR_DOMAIN . '/cancel.html',
         ]);
 
-        return $this->render('payment/index.html.twig', [
-            'controller_name' => 'PaymentController',
-        ]);
+        return new RedirectResponse($checkout_session->url);
     }
 }
